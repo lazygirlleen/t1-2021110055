198779ee580ed58771b3cf570a7c2df6d0047815
Sulis <146931473+lazygirlleen@users.noreply.github.com>
2024-10-11 14:28:23 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      './products/**/*.blade.php',
+      './products/**/*.js',
+      './products/**/*.vue',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
